a5875617b580a2537cb61d09d07d7a58d7b9fff4
libkenta <libkenta@gmail.com>
2011-10-25 03:14:29 +0900

    initial import

---
+++ lib/Acme/SingleQuote.pm
@@ -0,0 +1,39 @@
+package Acme::SingleQuote;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Acme::SingleQuote -
+
+=head1 SYNOPSIS
+
+  use Acme::SingleQuote;
+
+=head1 DESCRIPTION
+
+Acme::SingleQuote is
+
+=head1 AUTHOR
+
+libkenta E<lt>libkenta at gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) libkenta
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
